202027c5c6102d7ffbdefbc73c26e702c0fe8fde
melo0 <melo0@op.pl>
2017-07-24 12:40:28 +0200

    Update Ping.py
    I was add english comments

--- Ping.py
+++ Ping.py
@@ -1,6 +1,7 @@
 import os, re, time
 """
 sprawdzenie czy odpowiedz PING zwaraca 'Reply' i ustawienie wartosci dla agrumentu 'result'
+Verify that the PING response returns 'Reply' and sets the value for the 'result' object.
 """
 def ping(IP):
 	if re.search('Reply',os.popen('ping -n 1 {}'.format(IP)).read()) != None:
@@ -12,18 +13,22 @@ def ping(IP):
 if __name__ == '__main__':
 	"""
 	otawrcie pliku w trybie dodawania
+	Opening file in append mode
 	"""
 	plik = open('ping.txt', 'a')
 	"""
 	dopisanie bierzacej daty
+	appending the current date 
 	"""
 	plik.write(time.strftime("%Y-%m-%d %H:%M")+'\n')
 	"""
 	lista adresow do sprawdzenia
+	list of addresses to check
 	"""
-	lista = ['google.com','NieIstniejacyAdres.eu']
+	lista = ['google.com','NotExistingAddress.eu']
 	""""
 	petla zapisujaca do pliku 'ping.txt'
+	loop that writes to a file 'ping.txt'
 	"""
 	for i in lista:
 		plik.write('Ping from '+i+' is {}'.format(ping(i))+'\n')
