80212c4793baa759171bce34ddb57a463f10a422
melo0 <melo0@op.pl>
2017-08-10 14:25:36 +0200

    Update PingInWindow.py
    Ascetic version, without unnecessary buttons

--- PingInWindow.py
+++ PingInWindow.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from tkinter import * 
+from tkinter import ttk
 from tkinter import filedialog
 import tkinter.ttk
 import os, re, time
@@ -11,14 +12,15 @@ from tkinter.tix import Shell
 from email._header_value_parser import Terminal
 
 
-
 def koniec():
     okno.quit()
     okno.destroy()
 
+
 def przypisz():
     zrodlo.insert(END, tkinter.filedialog.askopenfilename())
     return
+
     
 def ping(host):
     proce = subprocess.Popen("ping -n 1 " + host)
@@ -27,20 +29,20 @@ def ping(host):
     else:
         return "Time: "+ time.strftime("%H:%M:%S")+" - " + host + "  - UP!!!\n"
 
+
 def for_workers(x):
     return ping(x)
+
 
 def lista_wynikow(result):
     lista.insert(END, result)
     
     
-
 def przetwarzanie(ad):
     pool=multiprocessing.Pool(processes = 16)
     for i in ad:
         pool.apply_async(for_workers, args = (i, ), callback = lista_wynikow)
        
-
 
 def sprawdz():
     plik1 = open(zrodlo.get(), "r")
@@ -54,17 +56,20 @@ def sprawdz():
 if __name__ == "__main__":
     okno = tkinter.Tk()
     okno.title("Program Ping")
-    okno.geometry("650x450")
-    zrodlo = tkinter.Entry(okno)
-    butt1 = tkinter.Button(okno, text = "  ...  ", command = lambda:przypisz())
-    lista = tkinter.Text(okno)
-    
-    butt2 = tkinter.Button(okno, text = "Sprawdź", command = lambda: sprawdz())
-    butt3 = tkinter.Button(okno, text = "Koniec", command = lambda: koniec())
+    okno.geometry("670x440")
+    ramka = ttk.Frame(okno, padding = (10,10,10,10))
+    zrodlo = tkinter.Entry(ramka)
+    lista = tkinter.Text(ramka)
+    menubar = Menu(okno)
+    filemenu = Menu(menubar, tearoff = 0)
+    editmenu = Menu(menubar, tearoff = 0)
+    filemenu.add_command(label = "Wczytaj plik", command = przypisz)
+    filemenu.add_command(label = "Zamknij", command = koniec)
+    editmenu.add_command(label = "Sprawdź", command = sprawdz)
+    menubar.add_cascade(label = "Plik", menu = filemenu)
+    menubar.add_cascade(label = "Opcje", menu = editmenu)
+    okno.config(menu = menubar)
     zrodlo.grid(row = 0, column = 0, stick = W+E)
-    butt1.grid(row = 0, column = 0, stick = E)
-    
     lista.grid(row = 1,  stick = W)
-    butt2.grid(row = 2, stick = W+E)
-    butt3.grid(row = 2, stick = E)
+    ramka.pack()
     okno.mainloop()
